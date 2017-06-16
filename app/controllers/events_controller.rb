71bf886f3b9afbca8ceff179693f6936e7749af5
Gkami <you@example.com>
2017-06-16 18:10:54 +0800

    redoing the member count

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -32,7 +32,7 @@ class EventsController < ApplicationController
   # GET /events/1
   # GET /events/1.json
   def show
-   @joined = EventUser.all
+   @joined = @joined_event_users_id
   end
 
   # GET /events/new
