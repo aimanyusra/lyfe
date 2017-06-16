4b1c14e7f693b8d90961c32e5ce9b7d0ff5e3b1a
Gkami <you@example.com>
2017-06-16 20:09:01 +0800

    event#index-the join will only appear when you signed in

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -32,7 +32,7 @@ class EventsController < ApplicationController
   # GET /events/1
   # GET /events/1.json
   def show
-   @joined = @joined_event_users_id
+    @joined = EventUser.where(event_id:params[:id]).count
   end
 
   # GET /events/new
