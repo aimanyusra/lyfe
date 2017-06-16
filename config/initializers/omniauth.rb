2b212a390c848a2e7fe0eddfe151c8db3dc61589
Aiman Yusra <aimanyusra@hotmail.com>
2017-06-16 16:37:03 +0800

    added and configured google omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+	# scraping data from FB to get user details for creation
 	provider :facebook, ENV["FACEBOOK_APP_KEY"], ENV["FACEBOOK_APP_SECRET"], scope: 'public_profile', info_fields: 'email, first_name, last_name, age_range, gender'
+	provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 end
 
  
