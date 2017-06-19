9a90421e14fd93035aac0e2d6e59400f5bfe7e77
Aiman Yusra <aimanyusra@hotmail.com>
2017-06-20 00:24:23 +0800

    configured js for carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= fullcalendar
+//= require jquery
+//= require jquery_ujs
+//= require fullcalendar
 //= require turbolinks
+//= require moment
+//= require jquery.slick
 //= require_tree .
