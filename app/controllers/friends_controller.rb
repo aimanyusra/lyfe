ce9abc880ee6109a8a8c86a4a9f84a29a9b96b36
choostanley <stanleychoo0522@hotmail.com>
2017-06-21 13:25:21 +0800

    friend feature should work now

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,4 +3,12 @@ class FriendsController < ApplicationController
 		Friend.create(firstid: current_user.id, secondid: params[:id], status: 0)
 		redirect_to events_path
 	end
+
+	def edit
+		@friend = Friend.find(params[:id])
+		@friend.status = 1
+		@friend.save
+		byebug
+		redirect_to events_path
+	end
 end
