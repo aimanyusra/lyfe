9f233cc00d7f750ad51b6268afb0b25f3d7701b4
Aiman Yusra <aimanyusra@hotmail.com>
2017-06-22 04:16:47 +0800

    fixed col names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620190925) do
+ActiveRecord::Schema.define(version: 20170621103235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,8 +51,6 @@ ActiveRecord::Schema.define(version: 20170620190925) do
   create_table "events", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
-    t.date     "start_date"
-    t.date     "end_date"
     t.string   "location"
     t.integer  "age_limit"
     t.integer  "price"
@@ -65,7 +63,6 @@ ActiveRecord::Schema.define(version: 20170620190925) do
     t.string   "state"
     t.datetime "start_time"
     t.datetime "end_time"
-    t.string   "state"
   end
 
   create_table "favorites", force: :cascade do |t|
@@ -85,6 +82,16 @@ ActiveRecord::Schema.define(version: 20170620190925) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "style_photos", force: :cascade do |t|
+    t.integer  "style_id"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "price"
+    t.string   "link"
+  end
+
   create_table "style_tags", force: :cascade do |t|
     t.integer  "style_id"
     t.integer  "tag_id"
@@ -100,6 +107,7 @@ ActiveRecord::Schema.define(version: 20170620190925) do
     t.integer  "user_id"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.string   "images",     default: [],              array: true
     t.index ["user_id"], name: "index_styles_on_user_id", using: :btree
   end
 
